06c51302e74c10deef0cb4c703001dca2b7f1262
Daniil K <danilkarachev234@mail.ru>
2025-12-31 01:03:27 +0500

    create Search

--- app/api/products/route.ts
+++ app/api/products/route.ts
@@ -1,7 +1,14 @@
 import { prismaClient } from "@/prisma/prisma-client";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 export async function GET() {
   const products = await prismaClient.product.findMany();
+  return NextResponse.json(products);
+}
+export async function POST(req: NextRequest) {
+  const body = await req.json();
+
+  const products = await prismaClient.product.create({ data: body });
+
   return NextResponse.json(products);
 }
---
+++ hooks/useClickOutside.ts
@@ -0,0 +1,16 @@
+import { RefObject, useEffect } from "react";
+
+export const useClickOutside = (
+  ref: RefObject<HTMLDivElement | null>,
+  setState: (b: boolean) => void
+) => {
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (ref.current && !ref.current.contains(event.target as Node)) {
+        setState(false);
+      }
+    };
+    document.addEventListener("click", handleClickOutside);
+    return () => document.removeEventListener("click", handleClickOutside);
+  }, []);
+};
