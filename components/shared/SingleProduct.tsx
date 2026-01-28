30d3b1b7e080a4d338c308c7e1a914cc0dc9b737
Daniil K <danilkarachev234@mail.ru>
2026-01-28 22:50:19 +0500

    update SingleProduct

--- components/shared/SingleProduct.tsx
+++ components/shared/SingleProduct.tsx
@@ -1,16 +1,19 @@
 "use client";
 
+import { IPropduct } from "@/types";
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 
 function SingleProduct({ id }: { id: number }) {
-  const { data } = useQuery({
+  const { data } = useQuery<IPropduct>({
     queryKey: ["Product"],
-    queryFn: () => axios.get(`/products/${id}`).then((res) => res.data),
+    queryFn: () =>
+      axios
+        .get(`http://localhost:3000/api/products/${id}`)
+        .then((res) => res.data),
   });
-  console.log(data);
 
-  return <div></div>;
+  return <div>{data?.name}</div>;
 }
 
 export default SingleProduct;
