a0ceac06fb0e41fc857f091b8a972116415a6a11
Daniil K <danilkarachev234@mail.ru>
2026-02-04 00:02:54 +0500

    create cart store

---
+++ store/useCart.ts
@@ -0,0 +1,44 @@
+import { IPropduct } from "@/types";
+import { create } from "zustand";
+
+interface ICart extends IPropduct {
+  count: number;
+}
+
+interface ICartStore {
+  cart: ICart[];
+  addToCart: (item: ICart) => void;
+  increaseCartItem: (id: number) => void;
+  decreaseCartItem: (id: number) => void;
+  deleteCartItem: (id: number) => void;
+}
+
+export const useCart = create<ICartStore>((set) => ({
+  cart: [],
+  addToCart: (newCartItem) => {
+    set((state) => {
+      if (state.cart.some((el) => el.id === newCartItem.id)) {
+        return state;
+      }
+      return { cart: [newCartItem, ...state.cart] };
+    });
+  },
+
+  increaseCartItem: (id) =>
+    set((state) => ({
+      cart: state.cart.map((el) =>
+        el.id === id ? { ...el, count: el.count++ } : el,
+      ),
+    })),
+  decreaseCartItem: (id) =>
+    set((state) => ({
+      cart: state.cart.map((el) =>
+        el.id === id
+          ? { ...el, count: el.count > 1 ? el.count-- : el.count }
+          : el,
+      ),
+    })),
+  deleteCartItem(id) {
+    set((state) => ({ cart: state.cart.filter((el) => el.id !== id) }));
+  },
+}));
