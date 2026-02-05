a19e2bd14aff98c117a0ecb8c61bac29e08871a6
Daniil K <danilkarachev234@mail.ru>
2026-02-05 23:51:15 +0500

    update ProductCard

--- components/shared/ProductCard.tsx
+++ components/shared/ProductCard.tsx
@@ -45,35 +45,9 @@ const ProductCard: FunctionComponent<ProductCardProps> = ({ product }) => {
         </p>
         {isInCart ? (
           <div className="flex gap-3 font-bold text-[1.25rem]">
-            <button onClick={handleDecCartItem} className="cursor-pointer">
-              <svg
-                width="38"
-                height="38"
-                viewBox="0 0 38 38"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <rect
-                  x="0.5"
-                  y="0.5"
-                  width="37"
-                  height="37"
-                  rx="14.5"
-                  fill="white"
-                  stroke="#FE5F00"
-                />
-                <path
-                  d="M24.4289 19L14.4766 19"
-                  stroke="#FE5F00"
-                  strokeWidth="1.5"
-                  strokeLinecap="round"
-                />
-              </svg>
-            </button>
-            <p className="flex items-center">{cartProductQuantity}</p>
             <button
-              onClick={handleIncCartItem}
-              className="cursor-pointer active:scale-95 transition-all"
+              onClick={handleDecCartItem}
+              className="group cursor-pointer"
             >
               <svg
                 width="38"
@@ -90,10 +64,43 @@ const ProductCard: FunctionComponent<ProductCardProps> = ({ product }) => {
                   rx="14.5"
                   fill="white"
                   stroke="#FE5F00"
+                  className="group-hover:fill-(--orange) "
+                />
+                <path
+                  d="M24.4289 19L14.4766 19"
+                  stroke="#FE5F00"
+                  strokeWidth="1.5"
+                  strokeLinecap="round"
+                  className="group-hover:stroke-white"
+                />
+              </svg>
+            </button>
+            <p className="flex items-center">{cartProductQuantity}</p>
+            <button
+              onClick={handleIncCartItem}
+              className="group cursor-pointer active:scale-95 transition-all"
+            >
+              <svg
+                width="38"
+                height="38"
+                viewBox="0 0 38 38"
+                // fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <rect
+                  x="0.5"
+                  y="0.5"
+                  width="37"
+                  height="37"
+                  rx="14.5"
+                  fill="white"
+                  stroke="#FE5F00"
+                  className="group-hover:fill-(--orange) "
                 />
                 <path
                   d="M24.4769 18.3223H20.1147V14.2508C20.1147 14.0709 20.0381 13.8983 19.9017 13.771C19.7654 13.6438 19.5804 13.5723 19.3876 13.5723C19.1948 13.5723 19.0099 13.6438 18.8735 13.771C18.7372 13.8983 18.6606 14.0709 18.6606 14.2508V18.3223H14.2983C14.1055 18.3223 13.9206 18.3938 13.7842 18.521C13.6479 18.6483 13.5713 18.8209 13.5713 19.0008C13.5713 19.1808 13.6479 19.3534 13.7842 19.4807C13.9206 19.6079 14.1055 19.6794 14.2983 19.6794H18.6606V23.7508C18.6606 23.9308 18.7372 24.1034 18.8735 24.2307C19.0099 24.3579 19.1948 24.4294 19.3876 24.4294C19.5804 24.4294 19.7654 24.3579 19.9017 24.2307C20.0381 24.1034 20.1147 23.9308 20.1147 23.7508V19.6794H24.4769C24.6697 19.6794 24.8546 19.6079 24.991 19.4807C25.1273 19.3534 25.2039 19.1808 25.2039 19.0008C25.2039 18.8209 25.1273 18.6483 24.991 18.521C24.8546 18.3938 24.6697 18.3223 24.4769 18.3223Z"
                   fill="#FE5F00"
+                  className="group-hover:fill-white"
                 />
               </svg>
             </button>
